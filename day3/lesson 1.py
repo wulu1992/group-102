fc16052feba24a9d98f15eabdfc7609bb01c8326
Giorgi Tsulukidze <gwulu1992@gmail.com>
2022-10-06 16:47:08 +0400

    davaleba 1

---
+++ day3/lesson 1.py
@@ -0,0 +1,8 @@
+name = "giorgi"   # str(string)
+surname = "tsulukidze" # str(string)
+age = 29 #int (integer)
+height= 179.5  #float
+professor = False  #boolean
+
+
+print("me var" + " " + name + " " + surname + " " + str(age) + " " + "wlis" + " " + str(height) + " " + str(professor))
