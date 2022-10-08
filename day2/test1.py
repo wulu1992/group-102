ff8291835bb5a8cc5056ef4e86ab5ab3cf34c9db
Giorgi Tsulukidze <gwulu1992@gmail.com>
2022-10-08 13:11:54 +0400

    test1

---
+++ day2/test1.py
@@ -0,0 +1,50 @@
+# name = "giorgi"
+
+# print(len(name))
+
+# my_sentence = """davalebas romelsac gvaZlevs cota chemitac unda 
+# davamato"""
+# print(my_sentence) #meore xazze gadmosvlas sami cali ბრჭყალი ჭირ
+
+#knows|_programming =True
+
+# if 10>5:
+#     print("hello")  #თუ პირობა ჭეშმარიტია მაშინ  დაპრინტე
+
+# ტაბის საშუალებით მოვახდინეთ INDENTATION
+
+#full_name = "giorgi tsulukidze"
+
+#print(len(full_name))
+
+#ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად
+
+#print(full_name[16])
+
+#print("k" not in full_name)
+#print(full_name[2:9]) # 2 dan 9 mde
+#print(full_name[2:9:3]) # 2dan 9 mde 3 iani bijiT
+
+#print(full_name[:5]) # o dan 5 mde
+#print(full_name[5:]) # 5 dan bolomde
+
+#print(full_name[-1:-6:-1])
+
+
+#სტრინგებს აქვთ მეთოდები
+# 1) სტრინგი. upper()
+# 2) len(სტრინგი)
+
+#print(full_name.upper())
+
+# name =",,,GIORGI"
+
+# #print(name.lower())
+# print(name.strip(","))  
+
+txt = ",,,,,rrttgg.....banana....rrr"
+
+x = txt.strip(",.grt")
+
+print(x)
+
